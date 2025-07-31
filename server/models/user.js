48ad906330642b04055a5459c4942ed8de2793e2
Md Arif <aatif149209@gmail.com>
2025-07-31 08:37:45 +0530

    resolving email not coming

--- server/models/user.js
+++ server/models/user.js
@@ -1,1 +1,28 @@
-c
+// models/User.js
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  _id: {
+    type: "String",
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+
+  image: {
+    type: String,
+    required: false, // optional profile image
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
