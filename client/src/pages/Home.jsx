437a69270526b8c97ec56e53c50352dd627d5ec6
Md Arif <75657124+codewitharif@users.noreply.github.com>
2025-10-11 03:19:31 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import HeroSlider from "../components/HeroSlider";
 import MovieListing from "../components/MovieListing";
 import WhyChooseUs from "../components/WhyChooseUs";
+import FAQSection from "../components/FAQSection";
 import { featuredMovies } from "../store/useMovieStore";
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
       <HeroSlider featuredMovies={featuredMovies} />
       <MovieListing />
       <WhyChooseUs />
+      <FAQSection />
     </div>
   );
 };
